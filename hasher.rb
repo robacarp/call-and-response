7b75fb0d6880b1984843f8750d0bbed467f45601
Robert L. Carpenter <robacarp@gmail.com>
2014-03-03 11:16:25 -0700

    fallback to randomness if config file is empty
    also warn myself

--- hasher.rb
+++ hasher.rb
@@ -2,7 +2,14 @@ require 'openssl'
 
 module Hasher
   class << self
-    lines = File.read('config.txt').split("\n")
+    lines = File.read('config.txt').split("\n") rescue Errno::ENOENT
+
+    if lines.nil? || ! lines.kind_of?(Array) || lines.length < 2
+      puts "\033[31m WARNING: Invalid or missing config.txt, loading token bases with random data. \033[0m"
+      puts "\033[31m          Clients will not be able to authenticate. \033[0m"
+      lines = [SecureRandom.hex(100), SecureRandom.hex(100)]
+    end
+
     SALT = lines[0]
     PEPPER = lines[1]
 
